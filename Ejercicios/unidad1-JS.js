462fc29d550b69d7d458e580489fc68fe10dc4e2
soso <avilasofia076@gmail.com>
2022-09-19 11:20:59 -0300

    Unidad 1 javascript

---
+++ Ejercicios/unidad1-JS.js
@@ -0,0 +1,33 @@
+/* 1. Escribe un programa de una sola línea que haga que aparezca en la pantalla un
+alert que diga “Hello World”.*/ 
+
+console.log("Hello world")
+
+
+/*2. Escribe un programa de una sola línea que escriba en la pantalla un texto que
+diga “Hello World” (document.write).
+3. Escribe un programa de una sola línea que escriba en la pantalla el resultado
+de sumar 3 + 5.
+4. Escribe un programa de dos líneas que pida el nombre del usuario con un
+prompt y escriba un texto que diga “Hola nombreUsuario”
+5. Escribe un programa de tres líneas que pida un número, pida otro número y
+escriba el resultado de sumar estos dos números.
+6. Escribe un programa que pida dos números y escriba en la pantalla cual es el
+mayor.
+7. Escribe un programa que pida 3 números y escriba en la pantalla el mayor de
+los tres.
+8. Escribe un programa que pida un número y diga si es divisible por 2
+9. Escribe un programa que pida una frase y escriba cuantas veces aparece la
+letra a
+10. Escribe un programa que pida una frase y escriba las vocales que aparecen
+11. Escribe un programa que pida una frase y escriba cuántas de las letras que
+tiene son vocales
+12. Escribe un programa que pida una frase y escriba cuántas veces aparecen
+cada una de las vocales
+13. Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o
+7 (sólo hay que comprobar si lo es por uno de los cuatro)
+14. Añadir al ejercicio anterior que nos diga por cual de los cuatro es divisible (hay
+que decir todos por los que es divisible)
+15. Escribir un programa que escriba en pantalla los divisores de un número dado
+16. Escribir un programa que escriba en pantalla los divisores comunes de dos
+números dados */
