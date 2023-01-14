0873b11ac0dade88d9f9b9fdaabfa63e7103d14a
soso <avilasofia076@gmail.com>
2023-01-14 09:44:28 -0300

    :smoking:

---
+++ Proyecto/javaScript/resources.js
@@ -0,0 +1,14 @@
+var element=`<nav class="nav-bar">
+            <ul class="links">
+                <li> <a href="about.html">Acerca de</a></li> 
+                <li><a href="contact.html">Contacto</a></li> 
+            </ul>           
+        </nav>
+        
+        <h1>La cocina de Juan</h1>
+`
+document.getElementById('idHeader').innerHTML= element;
+
+element=`<h4> Derechos reservados @2023</h4>
+`
+document.getElementById('idFooter').innerHTML= element;
